01e68e3b2c7e243ed85f50facd07dd4e10bb3e9a
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2017-02-22 14:42:36 -0500

    creates basic structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import Header from './components/Header'
+import About from './components/About'
+import Home from './components/Home'
+
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <div>
+      <Header />
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+    </div>
+  </Router>,
   document.getElementById('root')
-);
+)
