7a6da63f57f0745453d39ae69579d24d5287e298
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2017-02-23 17:50:39 -0500

    adds Header placeholder

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import './Header.css'
 
 const Header = () => {
   return (
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
-      <li><Link to="/services">Services</Link></li>
-      <li><Link to="/our-work">Our Work</Link></li>
-      <li><Link to="/contacts">Contacts</Link></li>
-    </ul>
+    <div className="Header">
+      <h1>Agire here</h1>
+    </div>
   )
 }
 
