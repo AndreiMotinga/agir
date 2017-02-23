792e4f3f8cf5b8195cb7b517d7b9422262a1d90e
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2017-02-23 17:57:52 -0500

    adds placeholder text

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -4,6 +4,10 @@ const About = () => {
   return (
     <div>
       <h2>About</h2>
+      <p>Lorem exercitationem provident voluptatem magni quis molestias, quae perferendis blanditiis. Incidunt quae incidunt voluptate tempora ducimus id. Facere itaque vel neque laudantium esse? Aut voluptatibus adipisci error molestias facilis eum!</p>
+      <p>Sit repudiandae incidunt quo dolorem dolore repudiandae sapiente, porro. Iure quibusdam corporis id accusantium nostrum! Quia distinctio et maiores fugit facilis provident quasi consequuntur aliquid. Aut fuga quia dolorem perferendis.</p>
+      <p>Sit eligendi pariatur ab commodi aut ullam quod! Aperiam cumque velit quod labore architecto! Incidunt saepe nobis ratione maxime sapiente natus dolor? Aperiam voluptatum unde hic dolor neque inventore. Tempora.</p>
+      <p>Amet fugit magnam natus saepe nulla ea! Tempore consequuntur doloribus adipisci architecto sunt iusto dolore animi dicta id esse. Corporis culpa fuga nulla debitis perferendis. Natus doloremque neque libero voluptates?</p>
     </div>
   )
 }
--- src/components/pages/Services/Services.jsx
+++ src/components/pages/Services/Services.jsx
@@ -4,6 +4,9 @@ const Services = () => {
   return (
     <div>
       <h2>Services</h2>
+      <p>Adipisicing optio temporibus eos asperiores dolor itaque necessitatibus consequatur numquam! Ex tempora aspernatur cumque odit numquam hic aspernatur amet temporibus? Recusandae amet ratione maxime quibusdam consectetur hic ipsum sint? Rem.</p>
+      <p>Adipisicing minus nobis commodi culpa earum? Dolorem necessitatibus ratione mollitia ipsa ad, excepturi ipsa officiis libero voluptates dolore error totam. Sunt quod porro ea omnis voluptatem! Repellat ratione eaque quis.</p>
+      <p>Lorem facilis velit ratione reprehenderit totam earum. Accusantium nisi ad voluptatem doloremque omnis labore temporibus facilis harum non minus! Officiis libero doloribus nobis expedita sed quibusdam temporibus officiis asperiores eveniet.</p>
     </div>
   )
 }
