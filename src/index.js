53a4f6df99402b8daa293bd74f266a3ef98163b7
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2017-02-23 17:44:12 -0500

    move css to the top

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'normalize.css' // todo: prost-prod either remove or move to proper place
+import './css/index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
@@ -6,6 +9,7 @@ import {
   Link
 } from 'react-router-dom'
 
+import Header from './components/Header'
 import Nav from './components/Nav'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
@@ -13,12 +17,11 @@ import Services from './components/pages/Services'
 import OurWork from './components/pages/OurWork'
 import Contacts from './components/pages/Contacts'
 
-import 'normalize.css' // todo: prost-prod either remove or move to proper place
-import './css/index.css';
 
 ReactDOM.render(
   <Router>
     <div>
+      <Header />
       <Nav />
       <Route exact path="/" component={Home}/>
       <Route path="/about" component={About}/>
