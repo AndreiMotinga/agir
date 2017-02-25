1fed6bb6ebe5e37f97003a5bef8870e33b961f51
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2017-02-24 19:44:29 -0500

    nav css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import './Header.css'
+import logo from '../../img/agir.png';
 
-const Header = () => {
-  return (
-    <div className="Header">
-    </div>
-  )
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.handleImageLoaded = this.handleImageLoaded.bind(this)
+  }
+
+  handleImageLoaded(e) {
+    e.target.className += 'is-up'
+  }
+
+  render () {
+    return (
+      <div className="Header">
+        <div className="Header_brand">
+          <div className="Header_agir">
+            <img
+              src={logo}
+              alt="Agir solutions"
+              onLoad={this.handleImageLoaded}
+            />
+          </div>
+          <div className="Header_title">
+            <h1>Agir</h1>
+            <small>security solutions inc</small>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Header
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,25 +4,29 @@ import './Nav.css'
 
 const Nav = () => {
   return (
-    <ul className="Nav">
-      <li><Link to="/">HOME</Link></li>
-      <li>
-        <Link to="/our-work">OUR WORK</Link>
-        <li><Link to="/our-work/photos">PHOTOS</Link></li>
-        <li><Link to="/our-work/videos">VIDEOS</Link></li>
-      </li>
-      <li>
-        <Link to="/services">SERVICES</Link>
-        <ul className="Nav_submenu">
-          <li><Link to="/services/security">Security</Link></li>
-          <li><Link to="/services/structured-wiring">Structure Wiring</Link></li>
-          <li><Link to="/services/window-treatment">Window Treatment</Link></li>
-          <li><Link to="/services/interior-design">Interior Design</Link></li>
-        </ul>
-      </li>
-      <li><Link to="/contacts">CONTACT US</Link></li>
-      <li><a href="//facebook.com">NEWS</a></li>
-    </ul>
+    <nav className="Nav">
+      <ul className="Nav_menu">
+        <li><Link to="/">HOME</Link></li>
+        <li>
+          <Link to="/our-work">OUR WORK</Link>
+          <ul className="Nav_menu_submenu">
+            <li><Link to="/our-work/photos">Photos</Link></li>
+            <li><Link to="/our-work/videos">Videos</Link></li>
+          </ul>
+        </li>
+        <li>
+          <Link to="/services">SERVICES</Link>
+          <ul className="Nav_menu_submenu">
+            <li><Link to="/services/security">Security</Link></li>
+            <li><Link to="/services/structured-wiring">Structure Wiring</Link></li>
+            <li><Link to="/services/window-treatment">Window Treatment</Link></li>
+            <li><Link to="/services/interior-design">Interior Design</Link></li>
+          </ul>
+        </li>
+        <li><Link to="/contacts">CONTACT US</Link></li>
+        <li><a href="//facebook.com">NEWS</a></li>
+      </ul>
+    </nav>
   )
 }
 
