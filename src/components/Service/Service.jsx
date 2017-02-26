ad616a6cf5671d78aa5c04c7eb6a3c88a9c1ad11
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2017-02-25 21:39:41 -0500

    service component

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import Panel from '../Panel'
 import './Service.css'
 
 const Service = (props) => {
   const { title, text } = props.service
   return (
     <div className="Service">
-      <h2>{title}</h2>
-      <p>{text}</p>
+      <Panel title={title}>
+        <p>{text}</p>
+      </Panel>
     </div>
   )
 }
