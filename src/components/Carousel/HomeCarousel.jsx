bd6459f5cd420b61f2225f906536f62a7bfb14b7
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2017-02-26 13:07:19 -0500

    adds carousel

--- src/components/Carousel/HomeCarousel.jsx
+++ src/components/Carousel/HomeCarousel.jsx
@@ -2,30 +2,36 @@ import React from 'react';
 import  { Carousel } from 'react-responsive-carousel'
 // import { Link } from 'react-router-dom'
 import 'react-responsive-carousel/lib/styles/carousel.css'
+import Service from '../Service'
+
+import slide1 from '../../img/carousel/slide-1.png'
+import slide2 from '../../img/carousel/slide-2.png'
+import slide3 from '../../img/carousel/slide-3.png'
 
 const HomeCarousel = () => {
+  const props = {
+    title: "Agir Security Solution Inc.",
+    text: "Founded in 2009, Agir Security Solution Inc. performs commercial, residential, industrial and service work throughout New York area and nearby states. No matter the electrical job, the experts at Agir are here to help. Our extremely knowledgeable technicians have a variety of electrical skills, including audio and video wiring expertise, to complete any electrical task quickly and professionally."
+  }
   return (
-      <div>
-        <Carousel
-          showArrows
-          showThumbs={false}
-          showStatus={false}
-          infiniteLoop
-          autoPlay
-        >
-          <div>
-            <img src="http://lorempixel.com/900/500/sports/1/" />
-            <p className="legend">Legend 1</p>
-          </div>
-          <div>
-            <img src="http://lorempixel.com/900/500/sports/2/" />
-            <p className="legend">Legend 2</p>
-          </div>
-          <div>
-            <img src="http://lorempixel.com/900/500/sports/3/" />
-            <p className="legend">Legend 3</p>
-          </div>
-        </Carousel>
+    <div>
+      <Carousel
+        showArrows
+        showThumbs={false}
+        showStatus={false}
+        infiniteLoop
+      >
+        <div>
+          <img src={slide1} />
+        </div>
+        <div>
+          <img src={slide2} />
+        </div>
+        <div>
+          <img src={slide3} />
+        </div>
+      </Carousel>
+      <Service service={props} />
     </div>
   )
 }
