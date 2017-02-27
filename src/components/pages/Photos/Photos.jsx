63d2bc065144655fb6f90d6a0e71ce9f4e5583d7
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2017-02-27 10:48:21 -0500

    disable all styles

--- src/components/pages/Photos/Photos.jsx
+++ src/components/pages/Photos/Photos.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
 
-import './Photos.css'
-
 const Photos = () => {
   const images = [
     {
