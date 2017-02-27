5ccf0708fe0c7407b3fd33eb2af4a102a43e3930
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2017-02-27 15:04:37 -0500

    footer scss

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,33 +4,30 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className="Footer">
-      <div className="Footer_header"></div>
+      <div className="Footer_address">
+        <p>
+          <a href="http://maps.google.com/?q=2755+Harway+Ave+Brooklyn+NY"
+             target="_blank" >
+            <em>2755 Harway Ave, Brooklyn, NY 11214</em>
+          </a>
+          <br />
+          Phone <a class="phone-mobile" href="tel:1-718-676-0564">1(718) 676-0564<br/></a>
+          Email <a href="mailto:agirssinc@gmail.com">agirssinc@gmail.com</a>
+        </p>
+      </div>
 
-      <div className="Footer_body">
-        <div className="Footer_body_address">
-          <p>
-            2755 Harway Ave. <br/>
-            Brooklyn, NY 11214
-          </p>
-          <p>
-            Phone: +1 (718) 676-0564<br/>
-            Email: agirssinc@gmail.com
-          </p>
+      <div className="Footer_links">
+        <div className="Footer_body_social">
+          <ul>
+            <li><a href="javscript:void(0)">fb</a></li>
+            <li><a href="javscript:void(0)">gl</a></li>
+            <li><a href="javscript:void(0)">tw</a></li>
+          </ul>
         </div>
-
-        <div className="Footer_body_links">
-          <div className="Footer_body_social">
-            <ul>
-              <li><a href="javscript:void(0)">fb</a></li>
-              <li><a href="javscript:void(0)">gl</a></li>
-              <li><a href="javscript:void(0)">tw</a></li>
-            </ul>
-          </div>
-          <div className="Footer_body_ezworks">
-            <small>
-              Designed by <a href="//ezworks.online">ezworks.online</a>
-            </small>
-          </div>
+        <div className="Footer_body_ezworks">
+          <small>
+            Designed by <a className="ezworks" href="//ezworks.online">ezworks.online</a>
+          </small>
         </div>
       </div>
     </div>
