13607ef5cead670e04cf6e8ce735c7abf9819ac8
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2017-02-27 11:16:14 -0500

    removes container

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
-import Container from './components/Container'
 import Home from './components/pages/Home'
 import Photos from './components/pages/Photos'
 import Videos from './components/pages/Videos'
@@ -20,13 +19,13 @@ ReactDOM.render(
     <div>
       <Header />
       <Nav />
-      <Container>
+      <main>
         <Route exact path="/" component={Home}/>
         <Route path="/our-work/photos" component={Photos}/>
         <Route path="/our-work/videos" component={Videos}/>
         <Route path="/contacts" component={Contacts}/>
         <Route path="/services/:id" component={Services}/>
-      </Container>
+      </main>
       <Footer />
     </div>
   </BrowserRouter>,
