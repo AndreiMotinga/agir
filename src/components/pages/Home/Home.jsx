aa67592b38c37cbd11e29f150de0795cb972a0b4
Andrei Motinga <andrew.motinga@gmail.com>
2017-04-06 18:41:41 -0400

    updates home page

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -29,11 +29,6 @@ class Home extends React.Component {
   // }
 
   render () {
-    const details = {
-      title: "Agir Security Solution Inc.",
-      text: "Founded in 2009, Agir Security Solution Inc. performs commercial, residential, industrial and service work throughout New York area and nearby states. No matter the electrical job, the experts at Agir are here to help. Our extremely knowledgeable technicians have a variety of electrical skills, including audio and video wiring expertise, to complete any electrical task quickly and professionally."
-    }
-
     let btnClass = 'btn -call-us'
     if (this.state.mounted) btnClass += ' -large'
 
@@ -42,92 +37,81 @@ class Home extends React.Component {
         <div className="Home_callBtn">
           <a href="tel:+1(718)676-0564" className={btnClass}>Give Us a Call</a>
         </div>
-        <Panel title={details.title} align="center">
-          {details.text}
+        <Panel title="WHO WE ARE" align="center">
+          <p>
+            "Founded in 2009, Agir Security Solution Inc. performs commercial, residential, industrial and service work throughout New York area and nearby states. No matter the electrical job, the experts at Agir are here to help. Our extremely knowledgeable technicians have a variety of electrical skills, including audio and video wiring expertise, to complete any electrical task quickly and professionally."
+          </p>
         </Panel>
-        <Carousel
-          showArrows
-          showThumbs={false}
-          showStatus={false}
-          infiniteLoop
-          dynamicHeight={false}
-          className="presentation-mode"
-        >
-          <div>
-            <div className="slide_text">
-              <h1 className="slide_title"><a href="/services/electrical">Electrical Work</a></h1>
-              <ul className="slide_list">
-                <li className="slide_list_item">Wiring & Power Distribution</li>
-                <li className="slide_list_item">Temporary Service & Service Upgrades</li>
-                <li className="slide_list_item">Panels, Switches, Receptacles, Lighting</li>
-                <li className="slide_list_item">Appliance & Specialized Equipment Wiring</li>
-                <li className="slide_list_item">Electrical Inspections</li>
-                <li className="slide_list_item">Emergency Repairs & Maintenance</li>
-              </ul>
-            </div>
-            {/* <img src={slide1} alt="Agir Security Solutions"/> */}
-          </div>
-          <div>
-            <div className="slide_text">
-              <h1 className="slide_title"><a href="/services/low-voltage">Low Voltage</a></h1>
-              <ul className="slide_list">
-                <li className="slide_list_item">Structured Cabling</li>
-                <li className="slide_list_item">Alarm Systems</li>
-                <li className="slide_list_item">Security Camera Systems</li>
-                <li className="slide_list_item">Intercom Systems</li>
-                <li className="slide_list_item">Access Control Systems</li>
-                <li className="slide_list_item">Audio/Video Systems</li>
-                <li className="slide_list_item">Telephone Systems</li>
-                <li className="slide_list_item">Paging Systems</li>
-                <li className="slide_list_item">Motorized Window Treatments</li>
-                <li className="slide_list_item">Central Vacuum Systems</li>
-                <li className="slide_list_item">Building Automation</li>
-              </ul>
-            </div>
-            {/* <img src={slide2} alt="Agir Security Solutions"/> */}
-          </div>
-          <div>
-            <div className="slide_text">
-              <h1 className="slide_title"><a href="/services/design-services">Design Services</a></h1>
-              <ul className="slide_list">
-                <li className="slide_list_item">Technology Consulting</li>
-                <li className="slide_list_item">Design and Engineering</li>
-                <li className="slide_list_item">Project Management</li>
-                <li className="slide_list_item">Space Planning</li>
-              </ul>
-            </div>
-            {/* <img src={slide3} alt="Agir Security Solutions"/> */}
-          </div>
-          <div>
-            <div className="slide_text">
-              <h1 className="slide_title"><a href="/services/it-services">IT Services</a></h1>
-              <ul className="slide_list">
-                <li className="slide_list_item">Repairs and Consulting</li>
-                <li className="slide_list_item">Network Deployment & Integration</li>
-                <li className="slide_list_item">Wireless Networks</li>
-                <li className="slide_list_item">VOIP Systems</li>
-                <li className="slide_list_item">Cloud Services</li>
-              </ul>
-            </div>
-            {/* <img src={slide3} alt="Agir Security Solutions"/> */}
-          </div>
-          <div>
-            <div className="slide_text">
-              <h1 className="slide_title"><a href="/services/home-office-automation">Home/Office Automation</a></h1>
-              <ul className="slide_list">
-                <li className="slide_list_item">Smarthome - Home Automation</li>
-                <li className="slide_list_item">Motorized Window Treatments</li>
-                <li className="slide_list_item">Pool/Spa Control</li>
-                <li className="slide_list_item">HVAC & Climate Control</li>
-                <li className="slide_list_item">Digital Signage</li>
-                <li className="slide_list_item">Custom Home Theaters</li>
-                <li className="slide_list_item">Smart Lighting Control</li>
-                <li className="slide_list_item">Intuitive Touchscreens & Remotes</li>
-              </ul>
-            </div>
-            <img src={slide3} alt="Agir Security Solutions"/>
-          </div>
-        </Carousel>
+
+        <div className="carousel-container">
+          <Panel title="WHAT WE DO" align="center">
+            <Carousel
+              showArrows
+              showThumbs={false}
+              showStatus={false}
+              infiniteLoop
+              dynamicHeight={false}
+              className="presentation-mode"
+            >
+              <div key="content-0" className="">
+                  <h4 className="slide_title"><a href="/services/electrical">Electrical Work</a></h4>
+                  <ul className="slide_list">
+                    <li className="slide_list_item">Appliance & Specialized Equipment Wiring</li>
+                    <li className="slide_list_item">Panels, Switches, Receptacles, Lighting</li>
+                    <li className="slide_list_item">Temporary Service & Service Upgrades</li>
+                    <li className="slide_list_item">Emergency Repairs & Maintenance</li>
+                    <li className="slide_list_item">Wiring & Power Distribution</li>
+                    <li className="slide_list_item">Electrical Inspections</li>
+                  </ul>
+              </div>
+
+              <div key="content-1" className="">
+                <h4 className="slide_title"><a href="/services/low-voltage">Low Voltage</a></h4>
+                <ul className="slide_list">
+                  <li className="slide_list_item">Alarm/Sercurity/Intercom Systems</li>
+                  <li className="slide_list_item">Audio/Video/Telephone Systems</li>
+                  <li className="slide_list_item">Motorized Window Treatments</li>
+                  <li className="slide_list_item">Central Vacuum Systems</li>
+                  <li className="slide_list_item">Building Automation</li>
+                  <li className="slide_list_item">Structured Cabling</li>
+                </ul>
+              </div>
+
+              <div key="content-2" className="">
+                <h4 className="slide_title"><a href="/services/design-services">Design Services</a></h4>
+                <ul className="slide_list">
+                  <li className="slide_list_item">Design and Engineering</li>
+                  <li className="slide_list_item">Technology Consulting</li>
+                  <li className="slide_list_item">Project Management</li>
+                  <li className="slide_list_item">Space Planning</li>
+                </ul>
+              </div>
+
+              <div key="content-3" className="">
+                <h4 className="slide_title"><a href="/services/it-services">IT Services</a></h4>
+                <ul className="slide_list">
+                  <li className="slide_list_item">Network Deployment & Integration</li>
+                  <li className="slide_list_item">Repairs and Consulting</li>
+                  <li className="slide_list_item">Wireless Networks</li>
+                  <li className="slide_list_item">Cloud Services</li>
+                  <li className="slide_list_item">VOIP Systems</li>
+                </ul>
+              </div>
+
+              <div key="content-3" className="">
+                <h4 className="slide_title"><a href="/services/home-office-automation">Home/Office Automation</a></h4>
+                <ul className="slide_list">
+                  <li className="slide_list_item">Intuitive Touchscreens & Remotes</li>
+                  <li className="slide_list_item">Smarthome - Home Automation</li>
+                  <li className="slide_list_item">HVAC & Climate Control</li>
+                  <li className="slide_list_item">Custom Home Theaters</li>
+                  <li className="slide_list_item">Pool/Spa Control</li>
+                  <li className="slide_list_item">Digital Signage</li>
+                </ul>
+              </div>
+            </Carousel>
+          </Panel>
+        </div>
       </div>
     )
   }
