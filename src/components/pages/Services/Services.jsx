ef7d571903a6b3d9329b91669ea8d90a20de5962
Andrei Motinga <andrew.motinga@gmail.com>
2017-04-06 14:02:14 -0400

    removes service.jsx

--- src/components/pages/Services/Services.jsx
+++ src/components/pages/Services/Services.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import Service from '../../Service'
 import data from './data.json'
+import Panel from '../../Panel'
+import './Services.css'
 
 const Services = ({ match }) => {
+  const url = "https://s3.amazonaws.com/ezworks-agir/services/"
   let service = data.categories.find((obj) => obj.id === match.params.id)
+  let { image, title, text } = service
+  if(image) image = <img className="Service_img" src={`${url}${image}`} alt={title}/>
 
   return (
-    <div className="Services">
-      <Service service={service} />
+    <div className="Service">
+      <Panel title={title}>
+        {image}
+        {text}
+      </Panel>
     </div>
   )
 }
