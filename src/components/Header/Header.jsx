590f706d749d4341bd6957d1529d570371a02b8a
Andrei Motinga <andrew.motinga@gmail.com>
2017-04-06 20:44:46 -0400

    remove phone from header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -39,11 +39,11 @@ class Header extends React.Component {
               <span className="name">Agir</span> <span>Security Solutions Inc</span>
             </h1>
           </Link>
-          <a className="Header_phone" href="tel:1-718-676-0564">
-            <h3>+1(718)676-0564</h3>
-            <h6>9:00 AM - 7:00 PM</h6>
-            <small></small>
-          </a>
+          {/* <a className="Header_phone" href="tel:1-718-676-0564"> */}
+          {/*   <h3>+1(718)676-0564</h3> */}
+          {/*   <h6>9:00 AM - 7:00 PM</h6> */}
+          {/*   <small></small> */}
+          {/* </a> */}
         </div>
         <div className="Header_borderBottom"></div>
       </div>
