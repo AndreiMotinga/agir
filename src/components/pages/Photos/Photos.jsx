b118b31479d93db0fc3ca70df5866ab43594d516
Andrei Motinga <andrew.motinga@gmail.com>
2017-04-19 14:24:45 -0400

    updates deploy address and photos url

--- src/components/pages/Photos/Photos.jsx
+++ src/components/pages/Photos/Photos.jsx
@@ -3,7 +3,8 @@ import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
 
 const Photos = () => {
-  const url = "https://s3.amazonaws.com/ezworks-agir/photos/"
+  const url = "https://s3-us-west-2.amazonaws.com/www.agirss.com/photos/"
+
   const images = [
     {
       original: `${url}1.jpg`,
